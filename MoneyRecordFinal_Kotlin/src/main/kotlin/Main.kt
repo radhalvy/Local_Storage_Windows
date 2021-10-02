3839a0f754e41070091adf801f691b03691c7840
Radhalvy <radhalvybernabel@gmail.com>
2021-10-02 16:55:30 -0400

    New function created and used as a new option inside second when statement. Also changed behaviour inside 'getBalance' function, specifically the 'file.forEachLine' part. Now the slices don't end at the last index, but to the first '>'. It gets the index of the first '>' and then subtracts 2 to that index to get the numeric value/amount.

--- MoneyRecordFinal_Kotlin/src/main/kotlin/Main.kt
+++ MoneyRecordFinal_Kotlin/src/main/kotlin/Main.kt
@@ -5,6 +5,7 @@ const val CREATE_NEW_ACCOUNT = 2
 const val DEPOSIT = 1
 const val WITHDRAWAL = 2
 const val GET_BALANCE = 3
+const val HISTORY = 4
 
 fun main() {
     println("1) Log in, 2) Create a new account")
@@ -17,7 +18,7 @@ fun main() {
             val filePath = "src/main/textFiles/${readStringLower()}.txt".replace("\\s".toRegex(), "_")
 
             if (doesExist(filePath)) {
-                println("1) Make a deposit, 2) Make a withdrawal, 3) Check account's total balance")
+                println("1) Make a deposit, 2) Make a withdrawal, 3) Check account's total balance, 4) See account's history")
                 val bankingOperation = try {readLine()!!.toInt()} catch (e: NumberFormatException) {null}
 
                 when (bankingOperation) {
@@ -33,6 +34,8 @@ fun main() {
                     }
 
                     GET_BALANCE -> println("$%,d".format(getBalance(filePath)))
+
+                    HISTORY -> println(getHistory(filePath))
 
                     else -> println("Invalid option. Try again.")
                 }
@@ -67,9 +70,9 @@ fun makeDeposit(filePath: String) {
     print("Amount to deposit: ")
     val amount = readLine()!!.toLong()
     val file = File(filePath)
-
     if (amount > 0) {
-        file.appendText("+ $amount\n")
+        print("Enter a note for this transaction: ")
+        file.appendText("+ $amount >>>> ${readLine()}\n")
     }
     else println("Amount cannot be zero nor negative.")
 }
@@ -79,25 +82,30 @@ fun makeWithdrawal(filePath: String) {
     val amount = readLine()!!.toLong()
     val accountBalance = getBalance(filePath)
     val file = File(filePath)
-
     if (amount <= 0) println("Amount cannot be zero nor negative.")
     else if (amount > accountBalance) println("There's not enough money in the account to complete the withdrawal.")
-    else file.appendText("- $amount\n")
+    else {
+        print("Enter a note for this transaction: ")
+        file.appendText("- $amount >>>> ${readLine()}\n")
+    }
 }
 
 fun getBalance(filePath: String): Long {
     val file = File(filePath)
     val upAmounts: MutableList<Long> = mutableListOf()
     val downAmounts: MutableList<Long> = mutableListOf()
-
     file.forEachLine {
         // Checks if it's a deposit or a withdrawal,
         // makes a slice of the amount and adds it to the corresponding list
-        if (it[0] == '+') upAmounts.add(it.slice(2..it.lastIndex).toLong())
-        else downAmounts.add(it.slice(2..it.lastIndex).toLong())
+        if (it[0] == '+') upAmounts.add(it.slice(2..it.indexOf(">") - 2).toLong())
+        else downAmounts.add(it.slice(2..it.indexOf(">") - 2).toLong())
     }
-
     return upAmounts.sum() - downAmounts.sum()
+}
+
+fun getHistory(filePath: String): String {
+    val file = File(filePath)
+    return file.readText()
 }
 
 fun doesExist(name: String): Boolean {
